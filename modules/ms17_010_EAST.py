1ed578eafb67d88d7bcd7ba5964a51ea8d8cb2cc
dm-ts <dennis_9393@hotmail.com>
2020-03-20 16:14:39 +0300

    comment

--- modules/ms17_010_EAST.py
+++ modules/ms17_010_EAST.py
@@ -2,7 +2,7 @@ from Sploit import Sploit
 from collections import OrderedDict
 from shellcodes.Shellcodes import OSShellcodes
 from impacket import smb, smbconnection
-from mysmb import MYSMB
+#from mysmb import MYSMB
 from struct import pack, unpack, unpack_from
 import sys
 import socket
